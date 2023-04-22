aca24ae33bb7e5d2b40024c555869d14d6fbb05e
Nima Balazadeh <n.balazadeh@email.kntu.ac.ir>
2023-04-22 12:28:31 +0430

    adding Start,Admin,User class and completing Start class

---
+++ src/main/java/ir/ac/kntu/Start.java
@@ -0,0 +1,37 @@
+package ir.ac.kntu;
+
+import java.util.Scanner;
+
+
+public class Start {
+    public static void adminOrUser(){
+        int option;
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("***********************************");
+        System.out.println("Login options:");
+        System.out.println("1-Admin");
+        System.out.println("2-User");
+        System.out.println("3-Exit.");
+        System.out.println("***********************************");
+        System.out.print("Please select your choice: ");
+
+        option = scanner.nextInt();
+        if (option==3){
+            System.exit(0);
+        }
+        while (option != 3) {
+            switch (option){
+                case 1:
+                    //Admin.adminLogin();
+                    break;
+                case 2:
+                    //User.userMenu();
+                    break;
+                default:
+                    System.out.println("Invalid choice!");
+                    break;
+            }
+        }
+        scanner.close();
+    }
+}
