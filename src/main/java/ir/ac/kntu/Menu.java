c29906fd86dbad8212e9c82281cb015789bfb8b0
Nima Balazadeh <n.balazadeh@email.kntu.ac.ir>
2023-04-22 16:36:39 +0430

    moving userOption method to Menu class

---
+++ src/main/java/ir/ac/kntu/Menu.java
@@ -0,0 +1,13 @@
+package ir.ac.kntu;
+
+public class Menu {
+    public static void userOption(){
+        System.out.println("***********************************");
+        System.out.println("User Menu:");
+        System.out.println("1-Sign in");
+        System.out.println("2-Sign up");
+        System.out.println("3-Back");
+        System.out.println("4-Exit.");
+        System.out.println("***********************************");
+    }
+}
--- src/main/java/ir/ac/kntu/User.java
+++ src/main/java/ir/ac/kntu/User.java
@@ -14,12 +14,6 @@ public class User {
 
     private static ArrayList<User> users = new ArrayList<>();
 
-
-    public User(String username, String password) {
-        this.username = username;
-        this.password = password;
-    }
-
     public String getPassword() {
         return password;
     }
@@ -36,13 +30,7 @@ public class User {
     }
 
     public static void userMenu() {
-        System.out.println("***********************************");
-        System.out.println("User Menu:");
-        System.out.println("1-Sign in");
-        System.out.println("2-Sign up");
-        System.out.println("3-Back");
-        System.out.println("4-Exit.");
-        System.out.println("***********************************");
+        Menu.userOption();
         int option;
         System.out.print("Please select your choice: ");
         Scanner scanner = new Scanner(System.in);
