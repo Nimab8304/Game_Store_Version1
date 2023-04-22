8d23a293ab82c285214d5ea07ebf9d5916a9a040
Nima Balazadeh <n.balazadeh@email.kntu.ac.ir>
2023-04-22 12:32:06 +0430

    adding adminLogin,goBack methods to Admin class

--- src/main/java/ir/ac/kntu/Admin.java
+++ src/main/java/ir/ac/kntu/Admin.java
@@ -1,4 +1,40 @@
 package ir.ac.kntu;
 
+import java.util.Scanner;
+
 public class Admin {
+    private static String username;
+    private static String password;
+
+    public Admin(String username, String password) {
+        this.username = username;
+        this.password = password;
+    }
+    public static void adminLogin(){
+        String usernameAsk,passwordAsk;
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Username: ");
+        usernameAsk = scanner.next();
+        System.out.print("password: ");
+        passwordAsk = scanner.next();
+        if (!usernameAsk.equals(username)){
+            System.out.println("Username is wrong");
+            goBack();
+
+        }else if (!passwordAsk.equals(password)){
+            System.out.println("Password is wrong");
+            goBack();
+        }
+    }
+    public static void goBack(){
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Do you want to try again:(y/n) ");
+        String answer;
+        answer = scanner.next();
+        if (answer.trim().equals("y")) {
+            Start.adminOrUser();
+        } else if (answer.trim().equals("n")) {
+            System.exit(0);
+        }
+    }
 }
