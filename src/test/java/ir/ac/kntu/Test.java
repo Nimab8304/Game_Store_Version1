0bf6c5acb61c66af2e4282956a868c0e6c6d3210
Nima Balazadeh <n.balazadeh@email.kntu.ac.ir>
2023-05-05 23:35:24 +0430

    Separating classes and make the code clean also added junit

--- src/test/java/ir/ac/kntu/Test.java
+++ src/test/java/ir/ac/kntu/Test.java
@@ -3,10 +3,19 @@ package ir.ac.kntu;
 import junit.framework.TestCase;
 
 public class Test extends TestCase {
-    public void testuser(){
+    public void testcountrate(){
         User user = new User("Amir", "Abasi123456", "zxjcbhj@gmail.com", 20, "24525");
         Game game = new Game("Snake", "very good", "action", 10);
         game.rates.put(user.getUsername(),7.2);
         assertEquals(UserLibrary.countRates(game),7.2);
+    }
+
+    public void testcountrates(){
+        User user = new User("Amir", "Abasi123456", "zxjcbhj@gmail.com", 20, "24525");
+        Game game = new Game("Walk", "very good", "action", 10);
+        User user1 = new User("She", "She123456", "zxyuybbhj@gmail.com", 30, "2125");
+        game.rates.put(user.getUsername(),7.2);
+        game.rates.put(user1.getUsername(),8.0);
+        assertEquals(UserLibrary.countRates(game),7.6);
     }
 }
