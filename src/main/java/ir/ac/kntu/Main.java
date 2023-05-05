0fcc27b3b07c4380632a8d16f5230a296eac1883
Nima Balazadeh <n.balazadeh@email.kntu.ac.ir>
2023-05-05 23:57:14 +0430

    seems to be the final version

--- src/main/java/ir/ac/kntu/Main.java
+++ src/main/java/ir/ac/kntu/Main.java
@@ -3,13 +3,13 @@ package ir.ac.kntu;
 public class Main {
     public static void main(String[] args) {
         Admin admin = new Admin("Nima", "123456");
-        User user = new User("Ali", "Abasi123456", "zxjcbhj@gmail.com", 20, "24525");
+        User user = new User("Abolfazl", "Abolfazl123456", "zxjcbhj@gmail.com", 100, "24525");
         Start.users.add(user);
-        User user1 = new User("She", "She123456", "zxyuybbhj@gmail.com", 30, "2125");
+        User user1 = new User("Shahram", "Shah123456", "zxyuybbhj@gmail.com", 130, "2125");
         Start.users.add(user1);
-        Game game = new Game("Snake", "very good", "action", 10);
-        Game game1 = new Game("walking dead", "khobe", "Drama", 11);
-        Game game2 = new Game("walker", "khob", "Dram", 8);
+        Game game = new Game("Pes2017", "you can play it with your friends", "action", 30);
+        Game game1 = new Game("Need For Speed", "looking for adrenalin", "Racing", 45);
+        Game game2 = new Game("Counter", "A shooter game", "Shooter", 27);
         //user.friends.add(user1);
         Start.games.add(game);
         Start.games.add(game1);
--- src/test/java/ir/ac/kntu/Test.java
+++ src/test/java/ir/ac/kntu/Test.java
@@ -18,4 +18,40 @@ public class Test extends TestCase {
         game.rates.put(user1.getUsername(),8.0);
         assertEquals(UserLibrary.countRates(game),7.6);
     }
+
+    public void testuser(){
+        User user = new User("Amir", "Abasi123456", "zxjcbhj@gmail.com", 20, "24525");
+        Game game = new Game("Walk", "very good", "action", 10);
+        User user1 = new User("She", "She123456", "zxyuybbhj@gmail.com", 30, "2125");
+        game.rates.put(user.getUsername(),7.2);
+        user.setWallet(50);
+        game.rates.put(user1.getUsername(),8.0);
+        assertEquals(user.getUsername(),"Amir");
+        assertEquals(user.getEmail(),"zxjcbhj@gmail.com");
+        assertEquals(user.getWallet(),50.0);
+    }
+
+    public void testuser1(){
+        User user = new User("Amir", "Abasi123456", "zxjcbhj@gmail.com", 20, "24525");
+        Game game = new Game("Walk", "very good", "action", 10);
+        User user1 = new User("She", "She123456", "zxyuybbhj@gmail.com", 30, "2125");
+        game.rates.put(user.getUsername(),7.2);
+        user.setWallet(50);
+        game.rates.put(user1.getUsername(),8.0);
+        assertEquals(user1.getUsername(),"She");
+        assertEquals(user1.getPassword(),"She123456");
+        assertEquals(user1.getPhoneNumber(),"2125");
+    }
+
+    public void testgame(){
+        User user = new User("Amir", "Abasi123456", "zxjcbhj@gmail.com", 20, "24525");
+        Game game = new Game("Walk", "very good", "action", 10);
+        User user1 = new User("She", "She123456", "zxyuybbhj@gmail.com", 30, "2125");
+        game.rates.put(user.getUsername(),7.2);
+        user.setWallet(50);
+        game.rates.put(user1.getUsername(),8.0);
+        assertEquals(game.getName(),"Walk");
+        assertEquals(game.getPrice(),10.0);
+        assertEquals(game.getGenres(),"action");
+    }
 }
